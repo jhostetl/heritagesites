6f9ed8b6790a1715d4207baa69c5e5558c42cdcd
John H <jhostetl@umich.edu>
2018-11-08 14:35:22 -0500

    asdf

--- urls.py
+++ urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path('sites/', views.SiteListView.as_view(), name='sites'),
     path('sites/<int:pk>/', views.SiteDetailView.as_view(), name='site_detail'),
 
-    path('country/', views.CountryAreaListView.as_view(), name='country_area'),
-    path('country/<int:pk>/', views.CountryAreaDetailView.as_view(), name='country_area_detail'),
-
+    path('countries/', views.CountryAreaListView.as_view(), name='countries'),
+    path('countries/<int:pk>/', views.CountryAreaDetailView.as_view(), name='country_area_detail'),
+	path('sites/new/', views.SiteCreateView.as_view(), name='site_new'),
+	path('sites/<int:pk>/delete/', views.SiteDeleteView.as_view(), name='site_delete'),
+	path('sites/<int:pk>/update/', views.SiteUpdateView.as_view(), name='site_update')
 ]
